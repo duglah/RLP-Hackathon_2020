662da9d8f0f057be32e98886d8b15eef13a03dc7
Philipp Manstein <philipp.gm@googlemail.com>
2020-11-13 23:26:54 +0100

    Add basic project setup

---
+++ SensorRegister/SensorRegister.Gui/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive.Concurrency;
+using ReactiveUI;
+using SensorRegister.Core.ViewModels;
+using Terminal.Gui;
+
+namespace SensorRegister.Gui
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			Application.Init();
+			RxApp.MainThreadScheduler = TerminalScheduler.Default;
+			RxApp.TaskpoolScheduler = TaskPoolScheduler.Default;
+			Application.Run(new LoginView(new LoginViewModel()));
+		}
+	}
+}
